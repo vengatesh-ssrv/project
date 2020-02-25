3f277fa1b1e8afb3d3807b6ccb3e396f22c90616
vengatesh_ssrv <36701062+vengatesh-ssrv@users.noreply.github.com>
2020-02-25 23:35:01 +0530

    Create emailalert.js

---
+++ jsFiles/emailalert.js
@@ -0,0 +1,25 @@
+(function(ctx){
+    if(ctx.Project == undefined){
+        ctx.Project = {};
+    }
+    function sendEmail() {
+        var myEmail;
+        var recipientEmail = [];
+        var subject;
+        var emailBody;
+        var password;
+        Email.send({
+        Host: "smtp.gmail.com",
+        Username : myEmail,
+        Password : password,
+        To : recipientEmail.join(","),
+        From : myEmail,
+        Subject : subject,
+        Body : emailBody,
+        }).then(
+            message => console.log("mail sent")
+        );
+        
+}
+ctx.Project.SendMail = sendEmail;
+})(this)
