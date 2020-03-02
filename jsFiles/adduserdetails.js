9a97733cd01be9978b2c366c37e962784468f144
vengatesh_ssrv <36701062+vengatesh-ssrv@users.noreply.github.com>
2020-03-02 14:07:17 +0530

    Update adduserdetails.js

--- jsFiles/adduserdetails.js
+++ jsFiles/adduserdetails.js
@@ -2,9 +2,9 @@
     var Project = ctx.Project;
     var patientdetails = Project.patientdetails;
     function addPatientdetails(){
-        patientdetails.name = document.getElementById("pname").nodeName;
-        patientdetails.email = document.getElementById("email").nodeValue;
-        patientdetails.doa = document.getElementById("Dateofadmission").nodeValue;
+        patientdetails.name = document.getElementById("pname").value;
+        patientdetails.email = document.getElementById("email").value;
+        patientdetails.doa = document.getElementById("Dateofadmission").value;
         document.getElementById("datavisualization").style.display = "";
     }
     Project.addPatientdetails = addPatientdetails;
