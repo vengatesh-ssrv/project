487688cbfa12463896dd7b1924d3c011602cb6c1
vengatesh_ssrv <36701062+vengatesh-ssrv@users.noreply.github.com>
2020-03-02 21:27:43 +0530

    push

--- jsFiles/adduserdetails.js
+++ jsFiles/adduserdetails.js
@@ -5,7 +5,6 @@
         patientdetails.name = document.getElementById("pname").value;
         patientdetails.email = document.getElementById("email").value;
         patientdetails.doa = document.getElementById("Dateofadmission").value;
-        document.getElementById("datavisualization").style.display = "";
     }
     Project.addPatientdetails = addPatientdetails;
 })(this)
